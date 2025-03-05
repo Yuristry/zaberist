83a070c6c3196a79e85dd942ae3fb7a056fcdf10
luxingyu <graylu516@163.com>
2025-03-05 16:30:21 +0800

    commit

---
+++ zaber-starter/src/main/java/org/zaber/starter/ZaberStarterApplication.java
@@ -0,0 +1,13 @@
+package org.zaber.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaberStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaberStarterApplication.class, args);
+	}
+
+}
